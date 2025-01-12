e875c261dd32d5c34fcb7d6aefc8535093a3598c
Shaoquan Chen <jerome_chen@hotmail.com>
2025-01-11 23:24:03 -0800

    git commit -m '<message>'

---
+++ Lab1.java
@@ -0,0 +1,21 @@
+public class Lab1 {
+    public class Vector3D {
+        private double x, y, z;
+
+        public Vector3D(double x, double y, double z) {
+            this.x=x;
+            this.y=y;
+            this.z=z;
+        }
+
+        public double getX(){
+            return x;
+        }
+        public double getY(){
+            return y;
+        }
+        public double getZ(){
+            return z;
+        }
+    }
+}
